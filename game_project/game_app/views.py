98df213c6a68efbfb785356d759a26d0fcb7a14e
yukiw33 <kanijsabado33@gmail.com>
2023-06-14 20:13:58 +0900

    test

--- game_project/game_app/views.py
+++ game_project/game_app/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'polls/index.html')
